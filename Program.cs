42741ee137740e277680d0d655667f279e000d6e
dev global <go.careers.fas@gmail.com>
2022-02-18 20:09:23 +0000

    All commited

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using RomanToInt.RomToInt;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RomanToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runConverter();
+        }
+
+        static void runConverter()
+        {
+
+            //Declares Class that converts from Decimal To Roman
+            InToRo2 dtr = new InToRo2();
+
+            //Draws line
+            drawLine();
+            Console.Write("Number: ");
+
+            //Checks if it Decimal Number
+            int num = checkValidator(Console.ReadLine());
+            if (num > 0)
+            {
+                drawLine();
+
+                Console.WriteLine(dtr.intToRoman(num));
+                Console.WriteLine(""); drawLine();
+
+
+                RoToIn rti = new RoToIn();
+
+                Console.Write("Insert Roman number: ");
+                string RNumber = Console.ReadLine().ToUpper();
+                
+                
+                if (checkVaLetters(RNumber))
+                {   
+                    string romanString = rti.romanToDecimal(RNumber).ToString();
+
+                    drawLine();
+                    Console.Write("Int number: ");
+                    Console.WriteLine(romanString);
+                    drawLine();
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Please enter letters only!");
+                }
+                
+            }
+
+        }
+
+
+        //Vlaidates if string is number
+        static int checkValidator(string container)
+        {
+
+            int num = 0;
+            try
+            {
+                
+                if (Regex.IsMatch(container, @"^\d+$"))
+                {
+                    num = Convert.ToInt32(container);
+                    //Console.WriteLine("Given string is numeric");
+
+                    if (num >= 1000)
+                    {
+                        Console.WriteLine("Out side range. Please enter number less than 1000");
+
+                    }
+                    else if (num <= 0)
+                    {
+                        Console.WriteLine("Please enter number greater than 0");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Given string is non-numeric");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return num;
+        }
+
+        //Validate if string is letter
+        static bool checkVaLetters(string container)
+        {
+
+            bool conIsString = false;
+            try
+            {
+
+                if (Regex.IsMatch(container, @"^[a-zA-Z]+$"))
+                {
+                    conIsString = true;
+                }                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return conIsString;
+        }
+
+        //draws the line
+        static void drawLine()
+        {
+            int i = 0;
+            while (i < 30)
+            {
+                Console.Write("-");
+                i++;
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
---
+++ RomToInt/RTI.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInt.RomToInt
+{
+    public class RTI
+    {
+
+    }
+
+    public class InToRo2
+    {
+        public String intToRoman(int num)
+        {
+            string ans="";
+            try
+            {
+                //Arrays of Roman Numbers
+                if(num<1000)
+                {
+                    String[] m = { "", "M", "MM", "MMM" };
+                    String[] c = { "",  "C",  "CC",  "CCC",  "CD",
+                       "D", "DC", "DCC", "DCCC", "CM" };
+                    String[] x = { "",  "X",  "XX",  "XXX",  "XL",
+                       "L", "LX", "LXX", "LXXX", "XC" };
+                    String[] i = { "",  "I",  "II",  "III",  "IV",
+                       "V", "VI", "VII", "VIII", "IX" };
+
+                    // Divides and assigns to responsible variable
+                    String thousands = m[num / 1000];
+                    String hundreds = c[(num % 1000) / 100];
+                    String tens = x[(num % 100) / 10];
+                    String ones = i[num % 10];
+
+                    ans = thousands + hundreds + tens + ones;
+                    return ans;
+                }
+                
+                
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
+            }
+
+            return ans;
+        }
+    }
+
+    public class RoToIn
+    {
+        //Returns Roman Number
+        public virtual int value(char r)
+        {
+            if (r == 'I')
+                return 1;
+            if (r == 'V')
+                return 5;
+            if (r == 'X')
+                return 10;
+            if (r == 'L')
+                return 50;
+            if (r == 'C')
+                return 100;
+            if (r == 'D')
+                return 500;
+            if (r == 'M')
+                return 1000;
+            return -1;
+        }
+
+        // Finds decimal value of a
+        // given romal numeral
+        public virtual int romanToDecimal(string str)
+        {
+            int res = 0;
+
+            try
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    // Getting value of symbol s[i]
+                    int s1 = value(str[i]);
+
+                    // Getting value of symbol s[i+1]
+                    if (i + 1 < str.Length)
+                    {
+                        int s2 = value(str[i + 1]);
+
+                        // Comparing both values
+                        if (s1 >= s2)
+                        {
+                            res = res + s1;
+                        }
+                        else
+                        {
+                            res = res + s2 - s1;
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        res = res + s1;
+                        i++;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            // Initialize result
+            
+
+            return res;
+        }
+
+    }
+}
